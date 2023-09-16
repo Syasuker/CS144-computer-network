a7ae43d4a74c367f94b5d1b7ef84a12d86b72b1e
syasuker <syasuker@gmail.com>
2023-09-16 13:16:09 +0800

    -

--- minnow-main/cmake-build-debug-remote-host/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /tmp/minnow-main/src
-# Build directory: /tmp/minnow-main/cmake-build-debug-remote-host/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- minnow-main/cmake-build-debug-remote-host/util/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /tmp/minnow-main/util
-# Build directory: /tmp/minnow-main/cmake-build-debug-remote-host/util
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
